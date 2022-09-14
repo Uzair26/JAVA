a978d435292e6574bd96a37bb8d1ca11d78356ab
Uzair26 <90671391+Uzair26@users.noreply.github.com>
2022-09-15 00:40:21 +0530

    Add files via upload

---
+++ StaticSquareFun.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Square{
+	static int number(int x){
+	return x*x;
+	
+} 
+}
+
+class Ques2{
+public static void main(String args[]){
+	Scanner inp = new Scanner(System.in);
+ 	int a = inp.nextInt();
+	int x = Square.number(a);
+	System.out.println(x);
+}
+}
+Footer
+© 2022 GitHub, Inc.
